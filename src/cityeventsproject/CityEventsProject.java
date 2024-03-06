4ca0488202002d09532d55d7a8efffd338b062eb
Bianca Ribeiro Mazotti <biancaribeiromazotti@gmail.com>
2024-03-06 14:32:18 +0000

    Project initialization and creation of the user class. 03/05/2024

---
+++ src/cityeventsproject/CityEventsProject.java
@@ -0,0 +1,14 @@
+package cityeventsproject;
+
+import model.User;
+
+public class CityEventsProject {
+
+    public static void main(String[] args) {
+        User eu = new User();
+        eu.creatUser();
+        eu.login();
+    }
+    
+    
+}
