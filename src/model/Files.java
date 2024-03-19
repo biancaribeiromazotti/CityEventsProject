ca38af02e5dd5a461d9653c41baeaa29e8a8fc7a
Bianca Ribeiro Mazotti <biancaribeiromazotti@gmail.com>
2024-03-19 10:57:40 +0000

    Add file writer and reader.

--- src/model/Files.java
+++ src/model/Files.java
@@ -1,5 +1,45 @@
 package model;
 
+import java.io.*;
+
 public class Files {
+
+    public static boolean writerFile (String path, String text){
+        try {
+            FileWriter file = new FileWriter(path);
+            PrintWriter writeFile = new PrintWriter(file);
+            writeFile.println(text);
+            writeFile.close();
+            return true;
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public static String readerFile (String pathFile) {
+        String fileContent = "";
+        try {
+            FileReader file = new FileReader(pathFile);
+            BufferedReader readerFile = new BufferedReader(file);
+            String line = "";
+            try {
+                line = readerFile.readLine();
+                while (line != null) {
+                    fileContent += line = "\n";
+                    line = readerFile.readLine();
+                }
+                file.close();
+                return fileContent;
+            } catch (IOException e) {
+                System.out.println("Unable to read the file.");
+                return "";
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found.");
+            return "";
+        }
+
+    }
 
 }
